deaad0a33d1cb42403f32c0911d8a3e0c2d36a1c
sophiehelf <sophie.helf@gmail.com>
2017-09-17 22:57:48 -0400

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+	playlist["Phil Ochs"] = "Here's to the State of Mississippi";
+}
+
+function removeFromPlaylist(playslit, artistName, songTitle) {
+	delete playlist["Slowdive"];
+}
